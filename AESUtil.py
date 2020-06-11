83fe96429b6fa4aa52ca71c5265457dfd3dfe51f
haakensonb <untamedhawk44@gmail.com>
2020-06-11 18:35:44 -0400

    made module for AES functions

---
+++ AESUtil.py
@@ -0,0 +1,15 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from constants import BLOCK_SIZE
+
+
+def encrypt(data, key, nonce=bytes([42])):
+    aes = AES.new(bytes.fromhex(key), AES.MODE_EAX, nonce=nonce)
+    ciphertext, _ = aes.encrypt_and_digest(pad(data.encode("utf-8"), BLOCK_SIZE))
+    return ciphertext.hex()
+
+
+def decrypt(data, key, nonce=bytes([42])):
+    aes = AES.new(bytes.fromhex(key), AES.MODE_EAX, nonce=nonce)
+    plaintext = aes.decrypt(bytes.fromhex(data))
+    return unpad(plaintext.decode("utf-8"), BLOCK_SIZE)
