f7a97a5d340533aebeb37efc63733f769a200714
haakensonb <untamedhawk44@gmail.com>
2020-06-13 22:47:20 -0400

    Updated comment

--- keys.py
+++ keys.py
@@ -2,8 +2,8 @@ from Crypto.PublicKey import RSA
 from utils import gen_random_hex_str
 
 
-# temporary global for testing keys
-# actual user keys will be stored in database
+# Temporary globals to hold keys for prototype version.
+# In the actual application user keys will be stored in the database.
 keyPairs = {'user1': RSA.generate(bits=3072), 'user2': RSA.generate(bits=3072)}
 sym_keys = {
     'user1': gen_random_hex_str(),
