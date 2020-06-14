21c310cc9b4a9e2fc6ee0577394f07f459ce0775
haakensonb <untamedhawk44@gmail.com>
2020-06-13 22:44:20 -0400

    Updated documentation

--- Provenance.py
+++ Provenance.py
@@ -9,6 +9,17 @@ from iv import IVs
 
 
 class Provenance:
+    """
+    The Provenance object provides a secure digital provenance record
+    chain for a document.
+
+    Attributes:
+        prov_id (str): unique str for identification
+        records (list[ProvenanceRecord]): list of provenance record objects
+        current_record (int): index value for current record
+        S (list[hexstr]): list of symmetric keys denoted as "S" in paper
+
+    """
     def __init__(self, prov_id, records=[], current_record=0, S=[]):
         self.prov_id = prov_id
         self.records = records
@@ -16,40 +27,47 @@ class Provenance:
         self.S = S
 
     def create_record(self, username, user_info, document):
-        # if there aren't any records
-        # then create a first record
+        """
+        Creates and adds a provenance record to the record chain.
+        Based on "Algorithm 1" in the paper.
+
+        Args:
+            username (str): string to identify user making record
+            user_info (str): any other app specific info about the user
+            document (str): document data that is to be secured
+
+        Returns:
+            void
+        """
+        # If there aren't any records, then create a first record.
         if not self.records:
-            # User 1 information
-            # using sym enc
+            # User 1 information.
             user_info = AESUtil.encrypt(user_info, sym_keys[username])
-            # use auditor key to enc sym key
+            # Use auditor's key to encrypt the user's symmetric key.
             encryptor = PKCS1_OAEP.new(auditor_keyPair.publickey())
             Si = encryptor.encrypt(bytes.fromhex(sym_keys[username]))
             self.S.append(Si)
-            # User 1 is creator of Document
+            # User 1 is the creator of the document.
             modifications = [Possible_Modification("created")]
-            # public keys are objects, but I'm not sure if
-            # this part should use a str representation of them instead?
             chain_info = [
                 auditor_keyPair.publickey(),
                 keyPairs[username].publickey()
             ]
             hashed_document = utils.hash_func(document)
-            # create checksum
+            # Create the checksum.
             modifications_str = "".join([x.value for x in modifications])
             operations = AESUtil.encrypt(modifications_str, sym_keys[username])
             checksum_data = utils.get_checksum_str(
                 user_info, operations, hashed_document, Si
             )
             checksum = RSAUtil.sign(keyPairs[username], checksum_data)
-            # create previous digital signature
-            # missing auditor IV
+            # Create the previous digital signature.
             chain_info_str = utils.get_chain_info_str(chain_info)
             prev_data = utils.get_iv_signature_str(
                 IVs[self.prov_id], chain_info_str, checksum
             )
             prev_record_signature = RSAUtil.sign(auditor_keyPair, prev_data)
-            # create provenance record and add to record list
+            # Create the provenance record and add it to the record list.
             self.records.append(ProvenanceRecord(
                 username,
                 user_info,
@@ -63,14 +81,13 @@ class Provenance:
 
         else:
             self.current_record += 1
-            # get the chain info from the previous record
-            # not sure if deepcopy is actually needed here
-            # shallow copy
+            # Get the chain info from the previous record.
+            # This uses a shallow copy, not sure if deep copy is needed.
             chain_info = self.records[self.current_record-1].chain_info[:]
-            # add the current user key to the chain
+            # Add the current user's public key to the chain.
             chain_info.append(keyPairs[username].publickey())
             chain_info_str = utils.get_chain_info_str(chain_info)
-            # create signature for previous record
+            # Create a signature for the previous record.
             prev_record = self.records[self.current_record-1]
             signature_str = utils.get_signature_str(
                 prev_record.hashed_document,
@@ -81,26 +98,25 @@ class Provenance:
                 keyPairs[username],
                 signature_str
             )
-            # update prev record's next value
+            # Update the previous record's next value.
             prev_record.next_record_signature = prev_record_signature
-            # user modifies document in some way
-            # then encrypt user info
+            # User then modifies the document in some way.
+            # After which we encrypt the user's info using their symmetric key.
             user_info = AESUtil.encrypt(user_info, sym_keys[username])
-            # list of operations performed
+            # The list of operations performed.
             modifications = [Possible_Modification("updated")]
             modifications_str = "".join([x.value for x in modifications])
             operations = AESUtil.encrypt(modifications_str, sym_keys[username])
             hashed_document = utils.hash_func(document)
-            # use auditor key to enc sym key
+            # Use the auditor's key to encrypt the user's symmetric key.
             encryptor = PKCS1_OAEP.new(auditor_keyPair.publickey())
             Si = encryptor.encrypt(bytes.fromhex(sym_keys[username]))
             self.S.append(Si)
-            # checksum_data = f"{user_info}{operations}{hashed_document}{Si.hex()}"
             checksum_data = utils.get_checksum_str(
                 user_info, operations, hashed_document, Si
             )
             checksum = RSAUtil.sign(keyPairs[username], checksum_data)
-            # create record
+            # Create the provenance record.
             self.records.append(ProvenanceRecord(
                 username,
                 user_info,
